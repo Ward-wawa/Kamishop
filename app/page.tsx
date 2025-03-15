30c197578dac30bc9cdd3f7ec080859b0708a37a
ward <wardistitan@gmail.com>
2025-03-15 16:46:04 +0300

    edited for build

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,20 @@ import Welcome from "@/components/Welcome";
 import {toast} from "react-toastify";
 import axiosInstance from "@/utils/axiosConfig";
 
-async function getPerfumes() {
-    try {
-        const response = await axiosInstance.get('/perfumes')
-
-        const data = await response.data
-        return data.data;
-    } catch (error) {
-        console.error('Failed to fetch perfumes:', error);
-        toast.error("Failed to fetch data");
-    }
-
-}
-
 export default async function Home() {
+
+    async function getPerfumes() {
+        try {
+            const response = await axiosInstance.get('/perfumes')
+
+            const data = await response.data
+            return data.data;
+        } catch (error) {
+            console.error('Failed to fetch perfumes:', error);
+            toast.error("Failed to fetch data");
+        }
+
+    }
     const perfumes = await getPerfumes();
   return (
     <>
