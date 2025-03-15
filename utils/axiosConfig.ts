af078df64bc5aee6c327d9ebc73f76795fe7ec78
ward <wardistitan@gmail.com>
2025-03-15 16:54:55 +0300

    edited for build

--- utils/axiosConfig.ts
+++ utils/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API}/api`,
+    baseURL: `${process.env.NEXTAUTH_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
     },
